32f430b70e69590055f60b2690a969ff75b95dcf
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:00:55 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "capacity_reservations_id" {
+  description = "Map of id values across all capacity_reservations, keyed the same as var.capacity_reservations"
+  value       = { for k, v in azurerm_capacity_reservation.capacity_reservations : k => v.id }
+}
 output "capacity_reservations_capacity_reservation_group_id" {
   description = "Map of capacity_reservation_group_id values across all capacity_reservations, keyed the same as var.capacity_reservations"
   value       = { for k, v in azurerm_capacity_reservation.capacity_reservations : k => v.capacity_reservation_group_id }
