cb599acc97305adfa3eed432cfd2a915acbb9ea4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "capacity_reservations" {
-  description = "All capacity_reservation resources"
-  value       = azurerm_capacity_reservation.capacity_reservations
-}
 output "capacity_reservations_capacity_reservation_group_id" {
-  description = "List of capacity_reservation_group_id values across all capacity_reservations"
-  value       = [for k, v in azurerm_capacity_reservation.capacity_reservations : v.capacity_reservation_group_id]
+  description = "Map of capacity_reservation_group_id values across all capacity_reservations, keyed the same as var.capacity_reservations"
+  value       = { for k, v in azurerm_capacity_reservation.capacity_reservations : k => v.capacity_reservation_group_id }
 }
 output "capacity_reservations_name" {
-  description = "List of name values across all capacity_reservations"
-  value       = [for k, v in azurerm_capacity_reservation.capacity_reservations : v.name]
+  description = "Map of name values across all capacity_reservations, keyed the same as var.capacity_reservations"
+  value       = { for k, v in azurerm_capacity_reservation.capacity_reservations : k => v.name }
 }
 output "capacity_reservations_sku" {
-  description = "List of sku values across all capacity_reservations"
-  value       = [for k, v in azurerm_capacity_reservation.capacity_reservations : v.sku]
+  description = "Map of sku values across all capacity_reservations, keyed the same as var.capacity_reservations"
+  value       = { for k, v in azurerm_capacity_reservation.capacity_reservations : k => v.sku }
 }
 output "capacity_reservations_tags" {
-  description = "List of tags values across all capacity_reservations"
-  value       = [for k, v in azurerm_capacity_reservation.capacity_reservations : v.tags]
+  description = "Map of tags values across all capacity_reservations, keyed the same as var.capacity_reservations"
+  value       = { for k, v in azurerm_capacity_reservation.capacity_reservations : k => v.tags }
 }
 output "capacity_reservations_zone" {
-  description = "List of zone values across all capacity_reservations"
-  value       = [for k, v in azurerm_capacity_reservation.capacity_reservations : v.zone]
+  description = "Map of zone values across all capacity_reservations, keyed the same as var.capacity_reservations"
+  value       = { for k, v in azurerm_capacity_reservation.capacity_reservations : k => v.zone }
 }
 
